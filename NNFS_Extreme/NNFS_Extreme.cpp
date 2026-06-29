f4adef9a14a5f5a0a885b08cd6b98443f577529e
generoustenacity <arawat3103@outlook.com>
2026-06-29 14:40:30 +0530

    Initial commit. setting up project structure

---
+++ NNFS_Extreme/NNFS_Extreme.cpp
@@ -0,0 +1,12 @@
+﻿// NNFS_Extreme.cpp : Defines the entry point for the application.
+//
+
+#include "NNFS_Extreme.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
