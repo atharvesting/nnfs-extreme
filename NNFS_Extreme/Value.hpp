0a35570fec5483fd2e3ebecbc2519b575881d079
generoustenacity <arawat3103@outlook.com>
2026-07-03 19:53:08 +0530

    Computational graph traversal + fixed operators - and /

--- NNFS_Extreme/Value.hpp
+++ NNFS_Extreme/Value.hpp
@@ -6,25 +6,21 @@
 #include <iostream> // cout, endl
 #include <functional> // function
 #include <cmath> // pow
+#include <set> // set
 
 class Value;
 
-using ValuePair = std::pair<std::shared_ptr<Value>, std::shared_ptr<Value>>;
+using ValuePtr = std::shared_ptr<Value>;
+using ValuePair = std::pair<ValuePtr, ValuePtr>;
 
 class Value : public std::enable_shared_from_this<Value> {
-private:
-	ValuePair _prev;
-	char _op;
-	std::function<void()> _backward = nullptr;
-
-	const std::shared_ptr<Value> _make(float data, ValuePair _children, char _op) const {
-		return std::make_shared<Value>(data, _children, _op);
-	}
-
 public:
 	float data;
 	float grad = 0.0f;
-
+	ValuePair _prev;
+	char _op;
+	std::function<void()> _backward = nullptr;
+	
 	Value(float data, ValuePair _children, char _op)
 		: data(data), _prev(_children), _op(_op) {
 	}
@@ -32,7 +28,11 @@ public:
 	Value(float data) : data(data), _prev{ nullptr, nullptr }, _op('\0') {}
 
 	void info() {
-		std::cout << "Value(" << data << ")" << std::endl;
+		std::cout << "Value(data=" << data << ", op=" << _op << ")" << std::endl;
+	}
+
+	const ValuePtr _make(float data, ValuePair _children, char _op) const {
+		return std::make_shared<Value>(data, _children, _op);
 	}
 
 	/*
@@ -43,25 +43,24 @@ public:
 	*	this pointer left-hand-side injection in operator overloads.
 	*/
 #define DEFINE_MIXED_OPERATOR(op_sym, op_char) \
-	friend std::shared_ptr<Value> operator op_sym(const std::shared_ptr<Value>& lhs, const std::shared_ptr<Value>& rhs) { \
+	friend ValuePtr operator op_sym(const ValuePtr& lhs, const ValuePtr& rhs) { \
 		return std::make_shared<Value>(lhs->data op_sym rhs->data, std::make_pair(lhs, rhs), op_char); \
 	} \
-	friend std::shared_ptr<Value> operator op_sym(const std::shared_ptr<Value>& lhs, const float& rhs) { \
+	friend ValuePtr operator op_sym(const ValuePtr& lhs, const float& rhs) { \
 		auto r_ptr = std::make_shared<Value>(rhs); \
 		return std::make_shared<Value>(lhs->data op_sym r_ptr->data, std::make_pair(lhs, r_ptr), op_char); \
 	} \
-	friend std::shared_ptr<Value> operator op_sym(const float& lhs, const std::shared_ptr<Value>& rhs) { \
+	friend ValuePtr operator op_sym(const float& lhs, const ValuePtr& rhs) { \
 		auto l_ptr = std::make_shared<Value>(lhs); \
 		return std::make_shared<Value>(l_ptr->data op_sym rhs->data, std::make_pair(l_ptr, rhs), op_char); \
 	}
 	DEFINE_MIXED_OPERATOR(+, '+')
-		DEFINE_MIXED_OPERATOR(-, '-')
-		DEFINE_MIXED_OPERATOR(*, '*')
-		DEFINE_MIXED_OPERATOR(/ , '/')
-
+	//DEFINE_MIXED_OPERATOR(-, '-')
+	DEFINE_MIXED_OPERATOR(*, '*')
+	//DEFINE_MIXED_OPERATOR(/ , '/')
 #undef DEFINE_MIXED_OPERATOR
 
-		std::shared_ptr<Value> pow(const float& exponent) {
+	ValuePtr pow(const float& exponent) {
 		auto expo_ptr = std::make_shared<Value>(exponent);
 		return std::make_shared<Value>(
 			std::pow(this->data, exponent),
@@ -70,9 +69,51 @@ public:
 		);
 	}
 
-	std::shared_ptr<Value> operator-() {
+	ValuePtr operator-() const {
 		return shared_from_this() * -1.0f;
+	}
+
+	ValuePtr operator-(ValuePtr other) const {
+		// Same as this + (-other)
+		return shared_from_this() + (other->operator-());
+	}
+
+	ValuePtr operator/(ValuePtr other) {
+		// Same as this * other**-1
+		return shared_from_this() * other->pow(-1);
 	}
 };
 
-std::shared_ptr<Value> newValue(const float& val);
+ValuePtr newValue(const float& val);
+
+using Edge = std::pair<ValuePtr, ValuePtr>;
+using NodeNedges = std::pair<
+	std::set<ValuePtr>, 
+	std::set<Edge>
+>;
+
+void trace_internal(ValuePtr root,
+					std::set<ValuePtr>& visited,
+					NodeNedges& result) {
+	if (!root || visited.count(root)) 
+		return;
+
+	visited.insert(root);
+	result.first.insert(root);
+
+	if (root->_prev.first != nullptr) {
+		result.second.insert({ root->_prev.first, root });
+		trace_internal(root->_prev.first, visited, result);
+	}
+	if (current->_prev.second != nullptr) {
+		result.second.insert({ root->_prev.second, root });
+		trace_internal(root->_prev.second, visited, result);
+	}
+}
+
+NodeNedges trace(ValuePtr root) {
+	NodeNedges result;
+	std::set<ValuePtr> visited;
+	trace_internal(root, visited, result);
+	return result;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# NNFS Extreme
+
+> NNFS from scratch. Actually.
+
+More about this project to be documented soon.
